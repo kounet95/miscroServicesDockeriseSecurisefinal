2a66e835a89e431f85ce0365e309199aca8a32e8
kounet95 <85202452+kounet95@users.noreply.github.com>
2024-09-13 11:09:48 -0400

    gg

--- apliMvcServe/src/main/java/org/example/aplimvcserve/repository/CustomerRepository.java
+++
@@ -1,8 +0,0 @@
-package org.example.aplimvcserve.repository;
-
-
-import org.example.aplimvcserve.entities.Customer;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CustomerRepository extends JpaRepository<Customer, Long> {
-}
